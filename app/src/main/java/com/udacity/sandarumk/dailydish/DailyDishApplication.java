3d88ea34b0a8d12e8d7da4c86bdb3847467872f9
Dinu Kumarasiri <sandarumk@gmail.com>
2018-06-10 16:11:44 +0100

    Initial Project Setup

---
+++ app/src/main/java/com/udacity/sandarumk/dailydish/DailyDishApplication.java
@@ -0,0 +1,19 @@
+package com.udacity.sandarumk.dailydish;
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+
+public class DailyDishApplication extends Application {
+
+    @Override public void onCreate() {
+        super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+        // Normal app init code...
+    }
+}
